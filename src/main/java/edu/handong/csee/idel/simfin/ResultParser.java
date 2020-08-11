4e1d6258e67dba3e0f244420aaab2a4c36ddfb7c
JC <jc@lifove.net>
2020-08-11 20:36:09 +0900

    Initial commit

---
+++ src/main/java/edu/handong/csee/idel/simfin/ResultParser.java
@@ -0,0 +1,143 @@
+package edu.handong.csee.idel.simfin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+public class ResultParser {
+	
+	enum Measure{Precision,Recall, F1};
+	Measure measure = Measure.F1;
+	double initialCutoff = 0.005;
+	double increment = 0.001;
+	double maxCutoff = 1.0;
+	
+	int kOffset = 2;
+	int maxK = 2;
+
+	public static void main(String[] args) {
+		
+		new ResultParser().run();
+
+	}
+
+	private void run() {
+		String resultFilePath = "data" + File.separator + "sqoop_result.csv";
+		
+		HashMap<String,ArrayList<SimFinData>> data = new HashMap<String,ArrayList<SimFinData>>();
+		ArrayList<String> keys = new ArrayList<String>();
+		
+		Reader in;
+		try {
+			in = new FileReader(resultFilePath);
+			Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+			for (CSVRecord record : records) {
+			    String commitID = record.get(0);
+			    String sourceFilePath = record.get(1);
+			    String key = commitID+sourceFilePath;
+			    keys.add(key);
+			    
+			    int rank = Integer.parseInt(record.get(6));
+			    Double distance = Double.parseDouble(record.get(7));
+			    int label = Integer.parseInt(record.get(9));
+			    
+			    SimFinData newRecord = new SimFinData(key, rank, distance, label);
+			    
+			    if(data.containsKey(key))
+			    	data.get(key).add(newRecord);
+			    else {
+			    	ArrayList<SimFinData> newSimFinData = new ArrayList<SimFinData>();
+			    	newSimFinData.add(newRecord);
+			    	data.put(key, newSimFinData);
+			    }
+			}
+			
+			System.out.print("cutoff");
+			for(int k=kOffset; k <= maxK; k++) {
+				System.out.print("," + k);	
+			}
+			System.out.println();	
+			
+			for(double cutoff = initialCutoff; cutoff <= maxCutoff; cutoff=cutoff+increment) {
+				//System.out.println(cutoff);
+				
+				System.out.print(cutoff);
+				for(int k=kOffset; k <= maxK; k++) {
+					//System.out.println(k);
+					System.out.print("," + evaluate(keys, data, kOffset,k, cutoff, measure));
+				}
+				System.out.println();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private double evaluate(ArrayList<String> keys, HashMap<String, ArrayList<SimFinData>> data, int kOffset, int k, double cutoff, Measure type) {
+
+		int TP = 0; // 1 -> 1
+		int FP = 0; // 0 -> 1
+		int TN = 0; // 0 -> 0
+		int FN = 0; // 1 -> 0
+		
+		for(String key:keys) {
+			
+			ArrayList<SimFinData> simFinData = data.get(key);			
+			double averagDistance = averageDistance(simFinData, kOffset, k);
+			
+			int label = simFinData.get(0).getLabel();
+			
+			if(label == 1) {
+				if(averagDistance<cutoff)
+					TP++;
+				else
+					FN++;
+			}
+			else {
+				if(averagDistance<cutoff)
+					FP++;
+				else
+					TN++;
+			}
+			
+		}
+		
+		double precision = TP/((double)TP+FP);
+		double recall = TP/((double)TP+FN);
+		double f1 = (2*precision*recall)/(precision+recall);
+		
+		if(type == Measure.F1)
+			return f1;
+		
+		if(type == Measure.Precision)
+			return precision;
+		
+		if(type == Measure.Recall)
+			return recall;
+		
+		return f1;
+	}
+
+	private double averageDistance(ArrayList<SimFinData> simFinData, int kOffset, int k) {
+		
+		double sum = 0.0;
+		
+		for(int i=kOffset-1; i < k; i++) {
+			sum = sum + simFinData.get(i).getDistance();
+		}
+		
+		return sum/(k-kOffset+1);
+	}
+
+}
---
+++ src/main/java/edu/handong/csee/idel/simfin/SimFinData.java
@@ -0,0 +1,39 @@
+package edu.handong.csee.idel.simfin;
+
+public class SimFinData {
+	private String key;
+	private int rank;
+	private double distance;
+	private int label;
+	
+	public SimFinData(String key2, int rank2, Double distance2, int label2) {
+		setKey(key2);
+		setRank(rank2);
+		setDistance(distance2);
+		setLabel(label2);
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	public int getLabel() {
+		return label;
+	}
+	public void setLabel(int label) {
+		this.label = label;
+	}
+}
