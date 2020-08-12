7f32c10d02bad16c973956bf5dbec2440b51a2a5
JC <jc@lifove.net>
2020-08-12 09:39:31 +0900

    Add test

---
+++ src/test/java/edu/handong/csee/idel/simfin/ResultParserTest.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.idel.simfin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ResultParserTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
