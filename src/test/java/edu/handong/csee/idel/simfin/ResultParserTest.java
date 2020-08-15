5b4d427e06dd5d8923af90ea5ab6a77038e4b75b
JC <jc@lifove.net>
2020-08-15 13:15:10 +0900

    Add sample driver

--- src/test/java/edu/handong/csee/idel/simfin/ResultParserTest.java
+++ src/test/java/edu/handong/csee/idel/simfin/ResultParserTest.java
@@ -7,7 +7,20 @@ import org.junit.Test;
 public class ResultParserTest {
 
 	@Test
-	public void test() {
+	public void testOpennlp() {
+		
+		String[] args = {"data/opennlp_result.csv", "0.01", "0.01", "1", "2"}; // 1: data path, 2: initial cutoff, 3: cutoff increment, 4: max cutoff, 5: target rank
+		
+		ResultParser.main(args);
+		
+	}
+	
+	@Test
+	public void testSentry() {
+		
+		String[] args = {"data/sentry_result.csv", "0.01", "0.01", "1", "2"}; // 1: data path, 2: initial cutoff, 3: cutoff increment, 4: max cutoff, 5: target rank
+		
+		ResultParser.main(args);
 		
 	}
 
